fc12f5f99e0248aa28ac2602d11d669d5f5d0fe7
anonys6 <shakibalam998@gmail.com>
2024-10-07 02:02:26 +0530

    Fix border style in navbar and testimonials components

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -5,8 +5,8 @@ import React from 'react'
 function page() {
     return (
         <>
-            <Testimonials />
             <AboutProgressBar />
+            <Testimonials />
         </>
     )
 }
