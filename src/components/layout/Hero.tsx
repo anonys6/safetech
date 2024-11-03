58d6f330377e8b90020a92b5365faaba07793a3d
anonys6 <shakibalam998@gmail.com>
2024-11-03 17:24:18 +0530

    add enhanced button

--- src/components/layout/Hero.tsx
+++ src/components/layout/Hero.tsx
@@ -1,13 +1,14 @@
-"use client";
+// "use client";
 
-import React, { useState } from 'react';
-import { Button } from '../ui/button';
-import { IoIosArrowForward, IoMdArrowForward } from "react-icons/io";
+// import React, { useState } from 'react';
+import { Button } from '../ui/button-enhanced';
+// import { IoIosArrowForward, IoMdArrowForward } from "react-icons/io";
 import { AnimatedShinyTextDemo } from './HeroShinyButton';
+import { ArrowRightIcon } from 'lucide-react';
 
 const Hero = () => {
-    const [isHovered, setIsHovered] = useState(false);
-    const [isHovered2, setIsHovered2] = useState(false);
+    // const [isHovered, setIsHovered] = useState(false);
+    // const [isHovered2, setIsHovered2] = useState(false);
 
     return (
         <div className="h-[calc(60vh-63px)] flex items-center justify-center font-bold flex-col gap-4 p-5 text-center lg:p-52 lg:h-[calc(100vh-63px)]">
@@ -16,18 +17,22 @@ const Hero = () => {
             <p className='text-xl z-10 font-normal drop-shadow-md text-gray-400'>Enhancing Safety and Efficiency in Workplaces Worldwide</p>
             <div className='flex gap-8 '>
                 <Button
+                    variant="expandIconGooeyLeft" Icon={ArrowRightIcon} iconPlacement="right"
                     className='z-10 font-semibold text-md'
-                    onMouseEnter={() => setIsHovered2(true)}
-                    onMouseLeave={() => setIsHovered2(false)}
+                // onMouseEnter={() => setIsHovered2(true)}
+                // onMouseLeave={() => setIsHovered2(false)}
                 >
-                    Get Started &nbsp; {isHovered2 ? <IoMdArrowForward size={20} /> : <IoIosArrowForward size={20} />}
+                    Get Started
+                    {/* &nbsp; {isHovered2 ? <IoMdArrowForward size={20} /> : <IoIosArrowForward size={20} />} */}
                 </Button>
                 <Button
-                    className='font-semibold text-md z-10 bg-transparent hover:bg-transparent hover:border-b-2 p-0 hover:border-orange-500 rounded-none'
-                    onMouseEnter={() => setIsHovered(true)}
-                    onMouseLeave={() => setIsHovered(false)}
+                    variant="linkHover2ExpandIcon" Icon={ArrowRightIcon} iconPlacement="right"
+                    className='font-semibold text-md z-10 bg-transparent hover:bg-transparent p-0  rounded-none'
+                // onMouseEnter={() => setIsHovered(true)}
+                // onMouseLeave={() => setIsHovered(false)}
                 >
-                    View Products &nbsp; {isHovered ? <IoMdArrowForward size={20} /> : <IoIosArrowForward size={20} />}
+                    View Products
+                    {/* &nbsp; {isHovered ? <IoMdArrowForward size={20} /> : <IoIosArrowForward size={20} />} */}
                 </Button>
             </div>
         </div>
